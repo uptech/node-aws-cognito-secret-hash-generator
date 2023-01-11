b9e94235853c78dfbad3179ac39af584d56df884
Cody Swartz <ctsstc@gmail.com>
2023-01-11 09:01:23 -0800

    Add awaits to questions

--- src/main.mjs
+++ src/main.mjs
@@ -3,9 +3,9 @@
 import { question } from "zx";
 import { getSecretHash } from "./helpers.mjs";
 
-const username = question("Username: ");
-const clientSecret = question("Client Secret: ");
-const clientId = question("Client ID: ");
+const username = await question("Username: ");
+const clientSecret = await question("Client Secret: ");
+const clientId = await question("Client ID: ");
 const secretHash = getSecretHash(username, clientSecret, clientId);
 
 console.log(`Secret Hash: ${secretHash}`)
